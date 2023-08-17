0855f3c6296d366a13f08048b5ddb441eaa5cd1c
moawezz <moawez.de@gmail.com>
2023-08-17 13:15:51 +0330

    Add Test for pages and account app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class SignUpPageTest(TestCase):
+    username = 'test_user'
+    email = 'test_email@gmail.com'
+    
+    def test_signup_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_signup_page_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_form(self):
+        UserModel = get_user_model()
+        user = UserModel.objects.create_user(
+            self.username,
+            self.email,
+        )
+        self.assertEqual(UserModel.objects.all().count(), 1)
+        self.assertEqual(UserModel.objects.all()[0].username, self.username)
+        self.assertEqual(UserModel.objects.all()[0].email, self.email)
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.urls import reverse_lazy
 
 from .forms import CustomUserCreationForm
 
-
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'

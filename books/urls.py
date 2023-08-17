6dac81c1e8fa23af937677bf53da0844ff304434
moawezz <moawez.de@gmail.com>
2023-08-17 19:11:26 +0330

    Add ListView and DetailView for Book

---
+++ books/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.BookListView.as_view(), name='books_list'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Book
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'books/books_list.html'
+    context_object_name = 'books'
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+

24a99a3ab8aa66436aa897b276bdb05ac31295e0
moawezz <moawez.de@gmail.com>
2023-08-18 13:58:05 +0330

    Edit books templates

--- books/views.py
+++ books/views.py
@@ -16,12 +16,12 @@ class BookDetailView(generic.DetailView):
 
 class BookCreateView(generic.CreateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price']
+    fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/create_book.html'
 
 class BookUpdateView(generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price']
+    fields = ['title', 'author', 'description', 'price', 'cover']
     template_name = 'books/book_update.html'
 
 class BookDeleteView(generic.DeleteView):
